192dfc597d5d4c0ff320b30139df5b09721c37a4
Pietro Della Notte <pdellanotte@alpenite.com>
2022-04-12 15:48:25 +0200

    add tax calc function

--- index.php
+++ index.php
@@ -28,6 +28,12 @@
 		    	echo "<p>".$key."</p>"; 
 		    }
 	    }
+        echo tax_calc(750,0.223);
+        function tax_calc($amount,$tax){
+            $calculate_tax = $amount*$tax;
+            $amount = round($amount+$calculate_tax,2);
+            return $amount;
+        }
     ?>
 </body>
 </html>
